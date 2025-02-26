65d98377e83f072073316d33a9b42fc2dad5c232
Pok Man <51484056+ph-u@users.noreply.github.com>
2025-02-26 15:00:30 +0000

    m ref & masterTemplate

--- containerized/masterTemplate.sh
+++ containerized/masterTemplate.sh
@@ -11,12 +11,13 @@
 # Note that NCBI accession number format: GCF_000... or GCA_000...
 # 1. txt file containing accession numbers of reference genomes, one line per number
 # 2. csv/txt file containing accession numbers (clinical/environmental isolates) that will become part of the blast database (can be downloaded using NCBI tool `datasets`)
-singularity pull ghcr.io/ph-u/dnds:latest
+apptainer pull docker://ghcr.io/ph-u/dnds_scan:latest
 
 ##### Stage 1: Reference genome & blastn database preparation #####
-
-#singularity run dnds ref relative_path_2_refGenome.txt
+apptainer run --bind ${PWD}:/data dnds_scan_latest.sif ref [../relative/path/2/refGenomes_list].txt
+apptainer run --bind ${PWD}:/data dnds_scan_latest.sif db [../relative/path/2/blastdb_accession_list].txt
 
 ##### Stage 2: Run dN/dS calculations #####
+apptainer run --bind ${PWD}:/data dnds_scan_latest.sif dnds [line number of ORF in iDx.csv file] [refAccessionNumber]
 
 exit
