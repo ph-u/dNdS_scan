91e38b39eb7f1621c230e65229cb2a2217f6a9a3
Pok Man <51484056+ph-u@users.noreply.github.com>
2025-03-01 08:38:20 +0000

    m enable multiple db chunks simultaneous download

--- containerized/masterTemplate.sh
+++ containerized/masterTemplate.sh
@@ -11,8 +11,7 @@
 # Note that NCBI accession number format: GCF_000... or GCA_000...
 # 1. txt file containing accession numbers of reference genomes, one line per number
 # 2. csv/txt file containing accession numbers (clinical/environmental isolates) that will become part of the blast database (can be downloaded using NCBI tool `datasets`)
-mkdir ../data
-apptainer pull docker://ghcr.io/ph-u/dnds_scan:latest
+mkdir ../data && apptainer pull docker://ghcr.io/ph-u/dnds_scan:latest
 
 ##### Stage 1: Reference genome & blastn database preparation #####
 apptainer run --bind ${PWD}:/data dnds_scan_latest.sif dbChunks [../relative/path/2/total_blastdb_accession_list].txt
