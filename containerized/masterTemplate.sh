caa02f194de84dd202ce13abb1086985bbe6e4f3
Pok Man <51484056+ph-u@users.noreply.github.com>
2025-03-08 03:07:25 +0000

    m flexible blastdb chunk size to avoid HPC memory overload

--- containerized/masterTemplate.sh
+++ containerized/masterTemplate.sh
@@ -2,7 +2,7 @@
 # author: ph-u
 # script: masterTemplate.sh
 # desc: template pipeline, need modifications/segregating sections
-# in: bash masterTemplate.sh [stage] [Group-Account-name] [../relative/path/2/refGenomes_list].txt [../relative/path/2/total_blastdb_accession_list].txt
+# in: bash masterTemplate.sh [stage] [Group-Account-name] [../relative/path/2/refGenomes_list].txt [../relative/path/2/total_blastdb_accession_list].txt [(stage 1 only) Chunk Size]
 # out: NA
 # arg: 4
 # date: 20250224
@@ -13,6 +13,8 @@
 gpNam=$2; refG=$3; totDB=$4
 
 if [[ ${sTage} -eq 1 ]];then
+  [[ -z $5 ]] && head -n 5 $0 | tail -n 1 && exit
+  ChunkSize=$5
 ##### Stage 0: Prepare accession lists #####
 # Note that NCBI accession number format: GCF_000... or GCA_000...
 # 1. txt file containing accession numbers of reference genomes, one line per number
@@ -21,18 +23,21 @@ if [[ ${sTage} -eq 1 ]];then
 
 ##### Stage 1: Reference genome & blastn database segregation #####
   apptainer run --bind ../data:/data dnds_scan_latest.sif ref ${refG} &
-  apptainer run --bind ../data:/data dnds_scan_latest.sif dbChunks ${totDB}
+  apptainer run --bind ../data:/data dnds_scan_latest.sif dbChunks ${totDB} ${ChunkSize}
 
 ##### Stage 2: blastn database preparation #####
 elif [[ ${sTage} -eq 2 ]];then
+  [[ -f blastdb-download.sh ]]&&rm blastdb-download.sh
+  p0=`echo -e "$ SLURM_ARRAY_TASK_ID" | sed -e "s/ //"`
 
-  for i in `ls ../data/*_${totDB}`;do
-      apptainer run --bind ../data:/data dnds_scan_latest.sif db ../data/${i} &
-  done
+  printf "Assembling blastdb-download run-script (`date`)"
+  echo -e "#!/bin/env bash\n# author: ph-u (docker container)\n# in: sbatch blastdb-download.sh\n# date: `date`\n#SBATCH -A ${gpNam}\n#SBATCH --nodes=1\n#SBATCH --ntasks=1\n#SBATCH --mail-type=NONE\n#SBATCH --requeue\n#SBATCH -p icelake-himem\n#SBATCH --time=12:00:00\n#SBATCH -J db_IN\n#SBATCH --array=1-`ls ../data/*_${totDB} | wc -l`\n\napptainer run --bind ${PWD}/../data:/data dnds_scan_latest.sif db ../data/${p0}_${totDB}" >> blastdb-download.sh
+  printf " -- Done (`date`)\n"
+  sbatch blastdb-download.sh
 
 else
 ##### Stage 3: Write & Run dN/dS calculations #####
-  printf "Assembling run-script (`date`)"
+  printf "Assembling dN/dS run-script (`date`)"
 
   [[ `ls | grep -e "dNdS_G" | grep -e ".sh" | wc -l` -gt 0 ]]&&rm dNdS_G*.sh
   p0=`echo -e "$ SLURM_ARRAY_TASK_ID" | sed -e "s/ //"`
@@ -46,6 +51,6 @@ else
   [[ -f dNdS_.sh ]]&&rm dNdS_.sh
 
   printf " -- Done (`date`)\n"
-  echo -e "Now you can run \` sbatch dNdS_xxx.sh \` to the SLURM system"
+  for i in `ls dNdS_*.sh`;do sbatch ${i};done
 fi
 exit
